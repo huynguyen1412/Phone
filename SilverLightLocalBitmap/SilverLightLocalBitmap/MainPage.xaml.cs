aec16e66b15b80e6975386bc9ea0fc726f525df1
Tony Romano <tonyrom777@gmail.com>
2011-11-21 15:19:58 -0800

    Initial install

---
+++ SilverLightLocalBitmap/SilverLightLocalBitmap/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using Microsoft.Phone.Tasks;
+
+
+namespace SilverLightLocalBitmap {
+    public partial class MainPage : PhoneApplicationPage {
+
+        CameraCaptureTask camera = new CameraCaptureTask();
+
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+
+            
+            camera.Completed += new EventHandler<PhotoResult>((sender, e) => {
+                if (e.TaskResult == TaskResult.OK) {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.SetSource(e.ChosenPhoto);    // set the bitmap image to the photo
+                    img.Source = bitmap;
+                }
+            });
+        }
+
+        protected override void OnManipulationStarted(ManipulationStartedEventArgs e) {
+
+            //// load the images from the XAP resources
+            //Uri imageUri = new Uri("Images/Hello.png", UriKind.Relative);
+            //StreamResourceInfo resourceInfo = Application.GetResourceStream(imageUri);
+            //BitmapImage bmp = new BitmapImage();
+            //bmp.SetSource(resourceInfo.Stream);
+
+            //// Set the Image element's source
+            //img.Source = bmp;
+
+            // User tapped the screen, start the camera task.
+            camera.Show();
+
+            // Handler cleanup
+            e.Complete();
+            e.Handled = true;
+
+            base.OnManipulationStarted(e);
+        }
+    }
+}
---
+++ SilverlightAccelerometer/SilverlightAccelerometer/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Devices.Sensors;
+using Microsoft.Xna.Framework;
+
+
+namespace SilverlightAccelerometer {
+
+    public partial class MainPage : PhoneApplicationPage {
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+
+            Accelerometer acc = new Accelerometer();
+            acc.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(OnAccelerometerReadingChanged);
+
+            try 
+	        {	        
+		        acc.Start();
+	        }
+	        catch (Exception e)
+	        {
+		        txtBlk.Text = e.Message;
+	        }
+        }
+
+
+        void OnAccelerometerReadingChanged(object sender, SensorReadingEventArgs<AccelerometerReading> args) {
+
+            String str = String.Format( "X = {0:F2}\n" +
+                                        "Y = {1:F2}\n" +
+                                        "Z = {2:F2}\n" +
+                                        "Magnitude = {3:F2}\n\n" +
+                                        "{4}",
+                                        args.SensorReading.Acceleration.X, args.SensorReading.Acceleration.Y, args.SensorReading.Acceleration.Z,
+                                        Math.Sqrt((args.SensorReading.Acceleration.X * args.SensorReading.Acceleration.X) +
+                                            (args.SensorReading.Acceleration.Y * args.SensorReading.Acceleration.Y) + 
+                                            (args.SensorReading.Acceleration.Z * args.SensorReading.Acceleration.Z)),
+                                        args.SensorReading.Timestamp);
+
+            if (txtBlk.CheckAccess()) {
+                txtBlk.Text = str;
+            }
+            else {
+                txtBlk.Dispatcher.BeginInvoke(()=> { 
+                    txtBlk.Text = str; });
+            }
+        }
+    }
+}
