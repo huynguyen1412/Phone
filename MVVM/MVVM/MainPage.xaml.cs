76c68bd590691f6c9e766e8a5e25151bba9c5d52
Tony Romano <tonyrom777@gmail.com>
2012-05-21 13:05:57 -0700

    Interim checkin

---
+++ MVVM/MVVM/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MVVM {
+    public partial class MainPage : PhoneApplicationPage {
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+
+            // simple way to bind the view to the view model   
+            this.DataContext = new ViewModel();
+        }
+    }
+}
