0f78dec3ba67dcad922e1d01d8582aeba0f49448
Tony Romano <tonyrom777@gmail.com>
2012-05-25 11:22:18 -0700

    Start of Project, added ViewModel and the Xaml

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+
+namespace IsolatedStorageDemo {
+    public class ViewModel : INotifyPropertyChanged {
+
+        public ViewModel() {
+            ImageUrl = "http://res1.newagesolution.net/Portals/0/twitter2_icon.jpg";
+        }
+
+        private String imageUrl;
+
+        public String ImageUrl {
+            get { 
+                return imageUrl; 
+            }
+            set { 
+                imageUrl = value;
+                RaisedPropertyChanged("ImageUrl");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisedPropertyChanged(String property) {
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null) {
+                handler(property, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
