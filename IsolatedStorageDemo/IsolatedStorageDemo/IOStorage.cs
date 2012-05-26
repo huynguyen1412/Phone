0ae881d4842b4a2f36c25e6ab2f8eeee2f76a2b5
Tony Romano <tonyrom777@gmail.com>
2012-05-25 22:34:58 -0700

    Moved Model from View model into IOStorage Class

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/IOStorage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace IsolatedStorageDemo {
+    public class IOStorage {
+
+        private Uri iOFilenameUri;
+        public Uri IOFilenameUri {
+            get { return iOFilenameUri; }
+            set { iOFilenameUri = value; }
+        }
+        
+        private String iOFilenameString;
+	    public String IOFilenameString {
+		    get { return iOFilenameString;}
+		    set { iOFilenameString = value;}
+	    }
+
+        private IOStorage(Uri filename) {
+        }
+        public IOStorage(String filename) {
+            IOFilenameString = filename;
+        }
+
+        public Stream Load() {
+
+            MemoryStream result = null;
+            if(String.IsNullOrEmpty(IOFilenameString)) {
+                throw new ArgumentNullException("IOFilenameString", "IO Filename parameter not set");
+            }
+
+            try {
+ 
+                using(IsolatedStorageFile file = IsolatedStorageFile.GetUserStoreForApplication()) {
+                    using(IsolatedStorageFileStream ioStream = file.OpenFile(IOFilenameString, FileMode.Open)) {
+
+                        result = new MemoryStream();
+                        ioStream.CopyTo(result);
+                    }
+                }
+            }
+            catch(Exception e) {
+            }
+
+            return result;
+        }
+
+        public void Save(Byte[] buffer, int bufferLength) {
+
+            if(String.IsNullOrEmpty(IOFilenameString)) {
+                throw new ArgumentNullException("IOFilenameString", "IO Filename parameter not set");
+            }
+
+            try {
+                if(!IsSpaceAvailble(bufferLength)) {
+                    throw new ArgumentOutOfRangeException("Buffer length parameter too long for Save");
+                }
+ 
+                using(IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(IOFilenameString, System.IO.FileMode.Create, IsolatedStorageFile.GetUserStoreForApplication())) {
+
+                    // Write it out to the file
+                    isfs.Write(buffer, 0, (int)bufferLength);
+                    isfs.Flush();
+                }
+            }
+            finally {
+            }
+        }
+
+        public bool IsSpaceAvailble(int size) {
+            using(IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication()) {
+                if(size > store.AvailableFreeSpace)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- IsolatedStorageDemo/IsolatedStorageDemo/ViewModel.cs
+++ IsolatedStorageDemo/IsolatedStorageDemo/ViewModel.cs
@@ -18,6 +18,8 @@ namespace IsolatedStorageDemo {
     public class ViewModel : INotifyPropertyChanged {
 
         WebClient webClient;
+        IOStorage phoneStorage;
+
         private ICommand getImage;
         private Uri imageUrl;
         public Uri ImageUrl {
@@ -55,13 +57,25 @@ namespace IsolatedStorageDemo {
         }
         public ViewModel() {
             ImageUrl = new Uri("http://res1.newagesolution.net/Portals/0/twitter2_icon.jpg");
+            phoneStorage = new IOStorage(ImageFileName);
             this.getImage = new DelegateCommand(GetImageFromUrl);
             webClient = new WebClient();
             LoadImageFromWebResult();
         }
 
         private void LoadImageFromWeb() {
-            webClient.OpenReadAsync(ImageUrl);
+            BitmapImage image = new BitmapImage();
+
+            try {
+                Stream result = phoneStorage.Load();
+                image.SetSource(result);
+            }
+            catch(Exception e) {
+                webClient.OpenReadAsync(ImageUrl);
+            }
+            finally {
+                ImageSource = image;
+            }
         }
 
         // Web IO
@@ -70,6 +84,8 @@ namespace IsolatedStorageDemo {
             // Get the image from the web
             webClient.OpenReadCompleted += (s1, e1) =>
                 {
+                    BitmapImage image = null;
+
                     try {
                         if(e1.Error == null) {
                             // read the image into memory
@@ -77,72 +93,17 @@ namespace IsolatedStorageDemo {
                             byte[] b = new byte[imgLength];
                             e1.Result.Read(b, 0, b.Length);
 
-                            BitmapImage image = new BitmapImage();
-
                             // Attempt to save the image to IsolatedStorage
-                            if(SaveImageStreamToIsolatedStorage(b, imgLength)) {
-                                image = LoadImageStreamFromIsolatedStorage();
-                            }
-
-                            if(image == null) {
-                                // Saving to storage failed, just set image manually
-                                image = new BitmapImage();
-                                image.SetSource(e1.Result);
-                            }
-
-                            ImageSource = image;
-                        }
+                            phoneStorage.Save(b, imgLength);
+                            image = new BitmapImage();
+                            image.SetSource(e1.Result);
+                        } 
                     }
                     catch(Exception e) {
                     }
+
+                    ImageSource = image;
                 };
-        }
-
-        // IsolateStorage 
-        private bool SaveImageStreamToIsolatedStorage(byte[] data, int length) {
-
-            bool result = false;
-            try {
-       
-                if(IsSpaceAvailble(length)) {
-                    using(IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(ImageFileName, System.IO.FileMode.Create, IsolatedStorageFile.GetUserStoreForApplication())) {
-
-                        // Write it out to the file
-                        isfs.Write(data, 0, length);
-                        isfs.Flush();
-                        result = true;
-                    }
-                }
-            }
-            catch(Exception e) {
-                MessageBox.Show(e.ToString());
-            }
-
-            return result;
-        }
-        private BitmapImage LoadImageStreamFromIsolatedStorage() {
-
-            BitmapImage result = null;
-
-            try {
-                using (IsolatedStorageFile file = IsolatedStorageFile.GetUserStoreForApplication()) {
-                    using(IsolatedStorageFileStream ioStream = file.OpenFile(ImageFileName, FileMode.Open)) {
-                        result = new BitmapImage();
-                        result.SetSource(ioStream);
-                    }
-                }
-            }
-            catch(Exception e) {
-            }
-
-            return result;
-        }
-        private bool IsSpaceAvailble(long amount) {
-            using(IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication()) {
-                 if (amount > store.AvailableFreeSpace)
-                     return false;
-            }
-            return true;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
