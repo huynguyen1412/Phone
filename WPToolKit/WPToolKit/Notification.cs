98893fdb0a505f0785eb6d7a8ce19764cc045630
Tony Romano <tonyrom777@gmail.com>
2012-06-15 15:57:27 -0700

    WIP

---
+++ WPToolKit/WPToolKit/Notification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WPToolKit
+{
+    public interface ITMessage
+    {
+    }
+
+  
+    public interface INotificationCenter
+    {
+        // The Action<T> method is the message sink
+        void Send(object sendTo, Action<ITMessage> messageHandler);
+        void Register(object receiver, Action<ITMessage> messageHandler);
+    }
+
+    public sealed class NotificationCenter : INotificationCenter {
+
+        private delegate void GenericMessage(object sender, ITMessage t);
+
+        public NotificationCenter() {
+        }
+
+        public void Send(object sendTo, Action<ITMessage> messageHandler) {
+        }
+
+        public void Register(object receiver, Action<ITMessage> messageHandler) {
+
+        }
+    }
+}
---
+++ WPToolKit/WPToolKitUnitTest/Unit Test/NotificationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPToolKit;
+
+namespace WPToolKitUnitTest
+{
+    [TestClass]
+    public class NotificationTest
+    {
+        NotificationCenter nc;
+
+        [TestInitialize]
+        public void SetUp() {
+
+            nc = new NotificationCenter();
+        }
+
+        [TestMethod]
+        public void TestNotificationSetup() {
+            nc.Register(this, MessageSink);
+
+            MessageSink();
+        }
+
+
+        public void MessageSink(ITMessage message) {
+        }
+    }
+}
