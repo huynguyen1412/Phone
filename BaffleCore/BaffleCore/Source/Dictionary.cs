e03c6642261f67687e744f221bd8b9f311be9833
Tony Romano <tonyrom777@gmail.com>
2012-08-10 17:17:40 -0700

    Added the dictionary
    Now can create the hash table with the dictionary.  Work done on
    separate thread.

---
+++ BaffleCore/BaffleCore/Source/Dictionary.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework;
+using System.ComponentModel;
+
+namespace BaffleCore.Source {
+   
+    public class Dictionary {
+            Dictionary<string, bool> _dictionaryHash;
+
+            public string[] Content { get; set; }
+            public Dictionary() {
+                Content = null;
+                _dictionaryHash = null;
+            }
+            public Dictionary<string, bool> DictionaryHash() {
+                return _dictionaryHash;
+            }
+            public void CreateDictionaryHash() {
+
+                BackgroundWorker dictionaryThread = new BackgroundWorker();
+                dictionaryThread.DoWork += new DoWorkEventHandler(ReadDictionaryStream);
+                dictionaryThread.RunWorkerCompleted += new RunWorkerCompletedEventHandler(ReadDictionaryComplete);
+                dictionaryThread.RunWorkerAsync();
+            }
+
+            private void ReadDictionaryStream(object sender, DoWorkEventArgs e) {
+                StreamReader stream = new StreamReader(TitleContainer.OpenStream("dictionary.dat"));
+                XmlSerializer xmlFormat = new XmlSerializer(typeof(string[]));
+
+                // Store object in a local file.
+                Content = xmlFormat.Deserialize(stream) as string[];
+
+                // create a hash the size of the dictionary
+                if (_dictionaryHash == null) {
+                    _dictionaryHash = new Dictionary<string, bool>(Content.Length);
+                }
+
+                foreach (string key in Content) {
+                    _dictionaryHash[key] = true;
+                }
+            }
+
+            private void ReadDictionaryComplete(object sender, RunWorkerCompletedEventArgs e) {
+            }
+        }
+}
---
+++ BaffleCore/BaffleUnitTest/Unit Test/DictionaryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BaffleCore;
+using BaffleCore.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaffleUnitTest.Unit_Test {
+
+    [TestClass]
+    public class DictionaryTest {
+
+        [TestMethod]
+        public void TestDictionaryCreate() {
+            Dictionary dict = new Dictionary();
+            dict.CreateDictionaryHash();
+        }
+    }
+}
